6aedba76176ec3f72d9d0a5dd0357eac3ea02d0c
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2025-02-12 11:39:06 +0100

    add PocketBase integration and update layout with dynamic page title

---
+++ pocketbase/pb_migrations/1739355013_updated_personne.js
@@ -0,0 +1,118 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1333826038")
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1819170229",
+    "max": 0,
+    "min": 0,
+    "name": "nom",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2787480667",
+    "max": 0,
+    "min": 0,
+    "name": "prenom",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(5, new Field({
+    "hidden": false,
+    "id": "select2663700287",
+    "maxSelect": 2,
+    "name": "nationalite",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "US",
+      "UK"
+    ]
+  }))
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2663334579",
+    "max": 0,
+    "min": 0,
+    "name": "lieu_naissance",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "date3467569840",
+    "max": "",
+    "min": "",
+    "name": "date_naissance",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "hidden": false,
+    "id": "date2274296624",
+    "max": "",
+    "min": "",
+    "name": "date_deces",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1333826038")
+
+  // remove field
+  collection.fields.removeById("text1819170229")
+
+  // remove field
+  collection.fields.removeById("text2787480667")
+
+  // remove field
+  collection.fields.removeById("select2663700287")
+
+  // remove field
+  collection.fields.removeById("text2663334579")
+
+  // remove field
+  collection.fields.removeById("date3467569840")
+
+  // remove field
+  collection.fields.removeById("date2274296624")
+
+  return app.save(collection)
+})
